5b6da2d135ed70e9cc6e70f1a7051fe2845ab189
andreassosilo <andreas.sosilo@gmail.com>
2019-07-28 13:23:49 +0700

    Add list of questions and randomize the question and choices

--- game.js
+++ game.js
@@ -1,31 +1,130 @@
 // Get reference for the question
 const question = document.getElementById('question')
-const choice = Array.from(document.getElementsByClassName('choice-text'))
+const choices = Array.from(document.getElementsByClassName('choice-text'))
 
 let currentQuestion = {}
-let acceptingAnswers = true
+let acceptingAnswers = false
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
   {
-    question: 'Inside which HTML element do we put the JavaScript??',
-    choice1: '<script>',
-    choice2: '<javascript>',
+    question: 'Who invented radio?',
+    choice1: 'Marconi',
+    choice2: 'Meucci',
     answer: 1
   },
   {
-    question:
-        "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script src='xxx.js'>",
+    question: 'Capital city of Australia?',
+    choice1: 'Canberra',
+    choice2: 'Sydney',
+    answer: 1
+  },
+  {
+    question: 'Potassium in periodic table?',
+    choice1: 'P',
+    choice2: 'K',
     answer: 2
   },
   {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alert('Hello World');",
+    question: 'Prime number?',
+    choice1: '37',
+    choice2: '39',
+    answer: 1
+  },
+  {
+    question: 'Where is Mount Kilimanjaro',
+    choice1: 'Kenya',
+    choice2: 'Tanzania',
     answer: 2
+  },
+  {
+    question: 'Who is Socrates?',
+    choice1: 'Inventor',
+    choice2: 'Philosopher',
+    answer: 2
+  },
+  {
+    question: 'Where is Eritrea?',
+    choice1: 'Asia',
+    choice2: 'Africa',
+    answer: 2
+  },
+  {
+    question: 'Primary colour in painting?',
+    choice1: 'Blue',
+    choice2: 'Green',
+    answer: 1
+  },
+  {
+    question: 'Who wrote Oliver Twist?',
+    choice1: 'Charles Dickens',
+    choice2: 'Mark Twain',
+    answer: 1
+  },
+  {
+    question: 'United Nations headquarter?',
+    choice1: 'New York',
+    choice2: 'Chicago',
+    answer: 1
+  },
+  {
+    question: 'Capsicum is a type of?',
+    choice1: 'Animal',
+    choice2: 'Plant',
+    answer: 2
+  },
+  {
+    question: 'Which airport is in Tokyo?',
+    choice1: 'Itami',
+    choice2: 'Haneda',
+    answer: 2
+  },
+  {
+    question: 'Who is Galileo Galilei?',
+    choice1: 'Astronomer',
+    choice2: 'Painter',
+    answer: 1
+  },
+  {
+    question: 'Winner of World Cup 2014?',
+    choice1: 'France',
+    choice2: 'Germany',
+    answer: 2
+  },
+  {
+    question: 'Einstein won Noble Prize in?',
+    choice1: 'Physics',
+    choice2: 'Peace',
+    answer: 1
   }
 ]
+
+// CONSTANTS
+const correctBonus = 10
+const maxQuestions = 3
+
+function startGame () {
+  questionCounter = 0
+  score = 0
+  availableQuestions = [...questions]
+  console.log(availableQuestions)
+  getNewQuestion()
+}
+
+function getNewQuestion () {
+  questionCounter++
+  // To make the questions randomize
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+  currentQuestion = availableQuestions[questionIndex]
+  question.innerText = currentQuestion.question
+
+  choices.forEach(choice => {
+    const number = choice.dataset['number']
+    choice.innerText = currentQuestion['choice' + number]
+  })
+}
+
+// Start the quiz app
+startGame()
