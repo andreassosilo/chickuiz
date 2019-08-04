31c8b4fd8bb3d7e90b0c1bc1e0fb2c486e9a13ef
andreassosilo <andreas.sosilo@gmail.com>
2019-08-04 20:56:12 +0700

    Modify service-worker.js and in index.html

--- service-worker.js
+++ service-worker.js
@@ -2,17 +2,7 @@
 let CACHE_NAME = 'my-site-cache-v1'
 // Progressive Web App: Update cache names any time any of the cached files change.
 let urlsToCache = [
-  '/chickuiz/offline.html',
-  '/chickuiz',
-  '/chickuiz/end.html',
-  '/chickuiz/game.html',
-  '/chickuiz/gameplay.html',
-  '/chickuiz/highscores.html',
-  '/chickuiz/app.css',
-  '/chickuiz/game.css',
-  '/chickuiz/highscores.css',
-  '/chickuiz/end.js',
-  '/chickuiz/game.js'
+  '/chickuiz/offline.html'
 ]
 
 // You need to define a callback for the install event and decide which files you want to cache.
@@ -20,44 +10,9 @@ self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Opened cache')
+      console.log('[ServiceWorker] Pre-caching offline page')
       return cache.addAll(urlsToCache)
     })
-  )
-})
-
-// Cache and return requests
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        // Cache hit - return response
-        if (response) {
-          return response
-        }
-
-        return fetch(event.request).then(
-          function (response) {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone()
-
-            caches.open(CACHE_NAME)
-              .then(function (cache) {
-                cache.put(event.request, responseToCache)
-              })
-
-            return response
-          }
-        )
-      })
   )
 })
 
@@ -73,5 +28,22 @@ self.addEventListener('activate', function (event) {
         }
       }))
     })
+  )
+})
+
+// Cache and return requests
+self.addEventListener('fetch', function (event) {
+  if (event.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return
+  }
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then((cache) => {
+            return cache.match('/chickuiz/offline.html')
+          })
+      })
   )
 })
